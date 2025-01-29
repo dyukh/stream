7ee532e788295b8b263aea2816ef940ce82dd8c1
dyukh <sobolevay@ipgg.sbras.ru>
2025-01-29 23:24:41 +0700

    Sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))  # Указывает на корень проекта
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Stream'
+copyright = '2025, Andrey Sobolev'
+author = 'Andrey Sobolev'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',  # Для извлечения docstrings
+    'sphinx.ext.viewcode',  # Для добавления ссылок на исходный код
+    'sphinx.ext.napoleon',  # Для поддержки Google- или NumPy-стиля docstrings
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. Stream documentation master file, created by
+   sphinx-quickstart on Wed Jan 29 23:20:24 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Stream documentation
+====================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
